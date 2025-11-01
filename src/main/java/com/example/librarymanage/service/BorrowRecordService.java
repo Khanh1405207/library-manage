33bf4f86fe9f1f2c411e0e2a64913bde595aaa86
Khanh1405207 <doanbaokhanh14052004@gmail.com>
2025-11-01 20:00:12 +0700

    update logic start-end borrow

--- src/main/java/com/example/librarymanage/service/BorrowRecordService.java
+++ src/main/java/com/example/librarymanage/service/BorrowRecordService.java
@@ -4,6 +4,7 @@ import com.example.librarymanage.model.Books;
 import com.example.librarymanage.model.BorrowRecord;
 import com.example.librarymanage.model.BorrowRecordId;
 import com.example.librarymanage.model.Borrower;
+import com.example.librarymanage.repository.BooksRepository;
 import com.example.librarymanage.repository.BorrowRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,8 @@ public class BorrowRecordService {
 
     @Autowired
     BorrowRecordRepository borrowRecordRepository;
+    @Autowired
+    BooksRepository booksRepository;
 
     public List<BorrowRecord> getAll(){
         return borrowRecordRepository.findAll();
@@ -37,6 +40,8 @@ public class BorrowRecordService {
     public void startBorrowRecord(Borrower borrower,Books book){
         LocalDateTime startTime= LocalDateTime.now();
         BorrowRecord record=new BorrowRecord(borrower,book,startTime,null);
+        book.setRemaining(book.getRemaining()-1);
+        booksRepository.save(book);
         borrowRecordRepository.save(record);
     }
 
@@ -45,6 +50,8 @@ public class BorrowRecordService {
         BorrowRecordId id=new BorrowRecordId(borrower.getId(),book.getId());
         BorrowRecord record=borrowRecordRepository.findById(id).get();
         record.setEndDate(endTime);
+        book.setRemaining(book.getRemaining()+1);
+        booksRepository.save(book);
         borrowRecordRepository.save(record);
     }
 }
