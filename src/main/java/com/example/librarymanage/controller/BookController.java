dc743ce774d246bc78f10b9126fe16833085047c
Khanh1405207 <doanbaokhanh14052004@gmail.com>
2025-11-02 19:22:14 +0700

    Add config - CORS config

--- src/main/java/com/example/librarymanage/controller/BookController.java
+++ src/main/java/com/example/librarymanage/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/book-api")
+@RequestMapping("/api")
 public class BookController {
 
     @Autowired
--- src/main/java/com/example/librarymanage/controller/BorrowRecordController.java
+++ src/main/java/com/example/librarymanage/controller/BorrowRecordController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/borrow-record-api")
+@RequestMapping("/api")
 public class BorrowRecordController {
 
     @Autowired
